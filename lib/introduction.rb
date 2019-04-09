81043f76e9d5c3f8dd0d3ab0b760f9e1f332cb7f
Maasa Kono <konomaasa@gmail.com>
2019-04-09 13:26:12 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+introduction("Josh")
+
+def introduction_with_language(name, program)
+  puts "Hi, my name is #{name} and I am learning to program in #{program}."
+end
